c07a79caf51b38e593450680b0d191d570973568
markdherrick-aurizon <mark.dherrick@aurizon.com.au>
2022-08-25 14:58:26 +0800

    Update composer

--- config/bundles.php
+++ config/bundles.php
@@ -6,6 +6,4 @@ return [
     Doctrine\Bundle\DoctrineBundle\DoctrineBundle::class => ['all' => true],
     Doctrine\Bundle\MigrationsBundle\DoctrineMigrationsBundle::class => ['all' => true],
     Symfony\Bundle\MakerBundle\MakerBundle::class => ['dev' => true],
-    Symfony\Bundle\TwigBundle\TwigBundle::class => ['all' => true],
-    Twig\Extra\TwigExtraBundle\TwigExtraBundle::class => ['all' => true],
 ];
